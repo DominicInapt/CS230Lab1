529d4ce284dbbc7843f1e724372c33abdabc4239
DominicInapt <97647397+DominicInapt@users.noreply.github.com>
2023-10-17 18:50:25 -0400

    Following the directions here

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section-002</h2>
+      <p>WVUI ID:800395463</p>
+      <p>Hi I am Dominic</p>
+    </div>
+  );
+}
+
+export default App;
